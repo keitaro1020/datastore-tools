903f6af4c1f2315dd11a1b79a3b011f98aabb2a8
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-22 16:08:58 +0900

    add truncate

---
+++ cmd/truncate.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"cloud.google.com/go/datastore"
+	"context"
+	"github.com/spf13/cobra"
+)
+
+func newTruncateCmd() *cobra.Command {
+	type Options struct {
+		OptProject   string
+		OptKind      string
+		OptNamespace string
+	}
+
+	var (
+		o = &Options{}
+	)
+
+	cmd := &cobra.Command{
+		Use:   "truncate",
+		Short: "Truncate Entity",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, err := datastore.NewClient(ctx, o.OptProject)
+			if err != nil {
+				return err
+			}
+
+			query := datastore.NewQuery(o.OptKind).KeysOnly()
+			if o.OptNamespace != "" {
+				query = query.Namespace(o.OptNamespace)
+			}
+
+			var entities []Entity
+
+			keys, err := client.GetAll(ctx, query, &entities)
+			if err != nil {
+				return err
+			}
+
+			count := len(keys)
+
+			tKeys, keys := slice(keys, 0, 500)
+			for len(tKeys) > 0 {
+				if err := client.DeleteMulti(ctx, tKeys); err != nil {
+					return err
+				}
+
+				tKeys, keys = slice(keys, 0, 500)
+			}
+			cmd.Printf("truncate finish, count = %d \n", count)
+
+			return nil
+		},
+		SilenceErrors: true,
+		SilenceUsage:  true,
+	}
+	cmd.Flags().StringVarP(&o.OptProject, "project", "p", "", "datastore project id [required]")
+	cmd.Flags().StringVarP(&o.OptKind, "kind", "k", "", "datastore kind [required]")
+	cmd.Flags().StringVarP(&o.OptNamespace, "namespace", "n", "", "datastore namespace")
+
+	cmd.MarkFlagRequired("project")
+	cmd.MarkFlagRequired("kind")
+
+	return cmd
+}
+
+func init() {
+	RootCmd.AddCommand(newTruncateCmd())
+}
+
+func slice(slice []*datastore.Key, start, end int) ([]*datastore.Key, []*datastore.Key) {
+	if len(slice) < start || len(slice) < end {
+		return slice, nil
+	}
+	ans := make([]*datastore.Key, (end - start))
+	copy(ans, slice[start:end])
+	slice = append(slice[:start], slice[end:]...)
+	return ans, slice
+}
