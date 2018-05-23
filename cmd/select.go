9165efe8d2e773c39218876b54eb6ef6e9dd4fd1
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-23 12:35:47 +0900

    output table

--- cmd/select.go
+++ cmd/select.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/spf13/cobra"
+	"github.com/olekukonko/tablewriter"
+	"fmt"
 )
 
 func newSelectCmd() *cobra.Command {
@@ -15,6 +17,7 @@ func newSelectCmd() *cobra.Command {
 		OptNamespace string
 		OptKeyFile   string
 		OptCount     bool
+		OptTable     bool
 	}
 
 	var (
@@ -48,18 +51,53 @@ func newSelectCmd() *cobra.Command {
 			}
 
 			if !o.OptCount {
-				for i, key := range keys {
-					entity := entities[i]
-					entity.Props["__key__"] = key
+				if o.OptTable {
+					if len(keys) > 0 {
+						headers := []string{"__key__"}
+						for propKey := range entities[0].Props {
+							headers = append(headers, propKey)
+						}
+						table := tablewriter.NewWriter(cmd.OutOrStdout())
+						table.SetHeader(headers)
+						table.SetRowLine(true)
 
-					j, _ := json.Marshal(entity.Props)
-					var ij bytes.Buffer
-					json.Indent(&ij, j, "", "  ")
-					js := ij.String()
-					if len(keys) > i+1 {
-						js += ","
+						for i, key := range keys {
+							entity := entities[i]
+							entity.Props["__key__"] = key
+
+							var row []string
+							for _, header := range headers {
+								if v, ok := entity.Props[header]; ok {
+									switch tv := v.(type) {
+									case *datastore.Key:
+										if tv.ID != 0 {
+											row = append(row, fmt.Sprintf("%v", tv.ID))
+										} else {
+											row = append(row, tv.Name)
+										}
+									default:
+										row = append(row, fmt.Sprintf("%v", tv))
+									}
+								}
+							}
+							table.Append(row)
+						}
+						table.Render()
 					}
-					cmd.Printf("%s\n", js)
+				} else {
+					for i, key := range keys {
+						entity := entities[i]
+						entity.Props["__key__"] = key
+
+						j, _ := json.Marshal(entity.Props)
+						var ij bytes.Buffer
+						json.Indent(&ij, j, "", "  ")
+						js := ij.String()
+						if len(keys) > i+1 {
+							js += ","
+						}
+						cmd.Printf("%s\n", js)
+					}
 				}
 			}
 			cmd.Printf("count: %d \n", len(keys))
@@ -72,8 +110,9 @@ func newSelectCmd() *cobra.Command {
 	cmd.Flags().StringVarP(&o.OptProject, "project", "p", "", "datastore project id [required]")
 	cmd.Flags().StringVarP(&o.OptKind, "kind", "k", "", "datastore kind [required]")
 	cmd.Flags().StringVarP(&o.OptNamespace, "namespace", "n", "", "datastore namespace")
-	cmd.Flags().StringVarP(&o.OptKeyFile, "key-file", "f", "", "gcp service account JSON key file")
+	cmd.Flags().StringVarP(&o.OptKeyFile, "key-file", "f", "", "gcp service account JSON key file [required]")
 	cmd.Flags().BoolVarP(&o.OptCount, "count", "c", false, "count only")
+	cmd.Flags().BoolVarP(&o.OptTable, "table", "t", false, "output table view")
 
 	cmd.MarkFlagRequired("project")
 	cmd.MarkFlagRequired("kind")
