aaa22f5bceb04452df2eb5afd748d2bfb0a6b0d2
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-28 12:06:02 +0900

    goroutine

--- cmd/truncate.go
+++ cmd/truncate.go
@@ -4,6 +4,7 @@ import (
 	"cloud.google.com/go/datastore"
 	"context"
 	"github.com/spf13/cobra"
+	"sync"
 )
 
 func newTruncateCmd() *cobra.Command {
@@ -50,25 +51,46 @@ func truncateFunction(cmd *cobra.Command, args []string) error {
 
 	count := len(keys)
 
-	tKeys, keys := slice(keys, 0, 500)
-	for len(tKeys) > 0 {
-		if err := client.Client.DeleteMulti(ctx, tKeys); err != nil {
-			return err
-		}
+	kss := slice(keys, 500)
+	wg := &sync.WaitGroup{}
 
-		tKeys, keys = slice(keys, 0, 500)
+	for _, ks := range kss {
+		wg.Add(1)
+		go func(keys []*datastore.Key) {
+			defer wg.Done()
+			if err := client.Client.DeleteMulti(ctx, keys); err != nil {
+				cmd.Printf("delete error: %+v", err)
+			}
+		}(ks)
 	}
+	wg.Wait()
 	cmd.Printf("truncate finish, count = %d \n", count)
 
 	return nil
 }
 
-func slice(slice []*datastore.Key, start, end int) ([]*datastore.Key, []*datastore.Key) {
-	if len(slice) < start || len(slice) < end {
-		return slice, nil
+func slice(slice []*datastore.Key, cnt int) [][]*datastore.Key {
+	if len(slice) <= cnt {
+		return [][]*datastore.Key{slice}
 	}
-	ans := make([]*datastore.Key, (end - start))
-	copy(ans, slice[start:end])
-	slice = append(slice[:start], slice[end:]...)
-	return ans, slice
+
+	size := len(slice) / cnt
+	if len(slice)%cnt > 0 {
+		size += 1
+	}
+
+	res := make([][]*datastore.Key, size)
+
+	for i := range res {
+		if len(slice) > cnt {
+			rt := make([]*datastore.Key, cnt)
+			copy(rt, slice[0:cnt])
+			slice = append(slice[:0], slice[cnt:]...)
+			res[i] = rt
+		} else {
+			res[i] = slice
+		}
+	}
+
+	return res
 }
